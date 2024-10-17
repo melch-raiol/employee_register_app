e0e8f4976f0f6523696c79e8dc41c223a347c741
melch-raiol <melchraiol@hotmail.com>
2024-10-17 11:44:19 -0300

    my first commit on this project of many...

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+    
+      <main >
+         <div>
+          <h1>div 1</h1>
+         </div>
+
+         <div>
+          <h2>div 2</h2>
+         </div>
+      </main>
+      
+  );
+}
